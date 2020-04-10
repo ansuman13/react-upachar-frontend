3c0062483fcdd4020509c93c5fde3793a28fa062
Ansuman Singh <ansumansingh@Ansumans-MacBook-Air.local>
2020-04-10 19:27:41 +0545

    first commit

---
+++ src/components/navbar/signedOutLinks/SignedOutLinks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignedOutLinks() {
+    return (
+        <>
+            <li className="nav-item"><button className="btn white z-depth-0">Signup</button> </li>
+            <li className="nav-item"><button className="btn white z-depth-0">Login</button> </li>
+        </>
+    )
+}
